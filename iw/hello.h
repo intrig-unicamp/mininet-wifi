bc8bf04c22de99e7262268740c29669dca9eaf4f
ramonfontes <ramonreisfontes@gmail.com>
2015-08-21 10:48:55 -0300

    1.0

--- iw/hello.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H_INCLUDED
-#define FUNCTIONS_H_INCLUDED
-/* ^^ these are the include guards */
-
-/* Prototypes for the functions */
-/* Sums two ints */
-int say_hello(int a, int b);
-
-#endif
