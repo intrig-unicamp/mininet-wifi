be68559a0c98b200239bb69de1e05fdc094f084e
Ramon <ramonreisfontes@gmail.com>
2015-11-30 23:50:05 -0200

    adding authentication

--- examples/wifiAuthentication.py
+++ examples/wifiAuthentication.py
@@ -15,9 +15,9 @@ def topology():
     net = Mininet( controller=Controller, link=TCLink, switch=OVSKernelSwitch )
 
     print "*** Creating nodes"
-    sta1 = net.addStation( 'sta1', passwd='123456789a', encrypt='wpa2' ) #security=(wpa,wpa2,wep)
-    sta2 = net.addStation( 'sta2', passwd='123456789a', encrypt='wpa2' ) #security=(wpa,wpa2,wep)
-    ap1 = net.addBaseStation( 'ap1', ssid="simplewifi", mode="g", channel="1", passwd='123456789a', encrypt='wpa2' ) #security=(wpa,wpa2,wep)
+    sta1 = net.addStation( 'sta1', passwd='123456789a', encrypt='wpa2' ) #encrypt=(wpa,wpa2,wep)
+    sta2 = net.addStation( 'sta2', passwd='123456789a', encrypt='wpa2' ) #encrypt=(wpa,wpa2,wep)
+    ap1 = net.addBaseStation( 'ap1', ssid="simplewifi", mode="g", channel="1", passwd='123456789a', encrypt='wpa2' ) #encrypt=(wpa,wpa2,wep)
     c0 = net.addController('c0', controller=Controller, ip='127.0.0.1', port=6633 )
 
     print "*** Associating Stations"
