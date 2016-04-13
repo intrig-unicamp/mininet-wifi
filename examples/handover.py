13e29a2882d96794cfb50752b0d9524c971d2890
Ramon <ramonreisfontes@gmail.com>
2016-04-13 09:15:43 -0300

    updating example

--- examples/handover.py
+++ examples/handover.py
@@ -19,7 +19,7 @@ def topology():
     print "*** Creating nodes"
     sta1 = net.addStation( 'sta1', mac='00:00:00:00:00:02', ip='10.0.0.2/8' )
     sta2 = net.addStation( 'sta2', mac='00:00:00:00:00:03', ip='10.0.0.3/8' )
-    ap1 = net.addBaseStation( 'ap1', ssid= 'new-ssid1', mode= 'g', channel= '1', position='15,50,0' )
+    ap1 = net.addBaseStation( 'ap1', ssid= 'new-ssid1', mode= 'g', channel= '1', position='15,80,0' )
     ap2 = net.addBaseStation( 'ap2', ssid= 'new-ssid2', mode= 'g', channel= '6', position='25,30,0' )
     c1 = net.addController( 'c1', controller=Controller )
 
@@ -35,13 +35,13 @@ def topology():
     ap2.start( [c1] )
 
     """uncomment to plot graph"""
-    #net.plotGraph(max_x=100, max_y=100)
+    net.plotGraph(max_x=100, max_y=100)
 
     net.startMobility(startTime=0)
     net.mobility('sta1', 'start', time=1, position='10,45,0')
-    net.mobility('sta2', 'start', time=2, position='10,40,0')
-    net.mobility('sta1', 'stop', time=12, position='15,10,0')
-    net.mobility('sta2', 'stop', time=22, position='25,25,0')
+    net.mobility('sta2', 'start', time=2, position='10,70,0')
+    net.mobility('sta1', 'stop', time=12, position='15,20,0')
+    net.mobility('sta2', 'stop', time=22, position='25,55,0')
     net.stopMobility(stopTime=23)
 
     print "*** Running CLI"
