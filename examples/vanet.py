298b571fc2704e02921d30c4198dddeb6e090777
Ramon <ramonreisfontes@gmail.com>
2016-11-12 14:50:23 -0200

    vanet example added

--- examples/vanet.py
+++ examples/vanet.py
@@ -10,6 +10,8 @@ from mininet.node import Controller,OVSKernelSwitch
 from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel
+import os
+import random
 
 def topology():
 
@@ -17,44 +19,80 @@ def topology():
     net = Mininet( controller=Controller, link=TCLink, switch=OVSKernelSwitch )
 
     print "*** Creating nodes"
-    car1 = net.addVehicle( 'car1', wlans=2, mac='00:00:00:00:00:01', ip='10.0.0.1/8', min_speed=1, max_speed=5 )
-    car2 = net.addVehicle( 'car2', wlans=2, mac='00:00:00:00:00:02', ip='10.0.0.2/8', min_speed=5, max_speed=10 )
-    car3 = net.addVehicle( 'car3', mac='00:00:00:00:00:03', ip='10.0.0.3/8', min_speed=10, max_speed=15 )
-    car4 = net.addVehicle( 'car4', mac='00:00:00:00:00:04', ip='10.0.0.4/8', min_speed=15, max_speed=20 )
-    car5 = net.addVehicle( 'car5', mac='00:00:00:00:00:05', ip='10.0.0.5/8', min_speed=15, max_speed=20 )
-    bs1 = net.addBaseStation( 'BS1', ssid= 'new-ssid1', mode= 'g', channel= '1' )
-    bs2 = net.addBaseStation( 'BS2', ssid= 'new-ssid2', mode= 'g', channel= '6' )
-    bs3 = net.addBaseStation( 'BS3', ssid= 'new-ssid3', mode= 'g', channel= '11' )
+    car = []
+    stas = []
+    for x in range(0, 10):
+        car.append(x)
+        stas.append(x)
+    for x in range(0, 10):
+        min = random.randint(1,10)
+        max= random.randint(11,30)
+        car[x] = net.addCar('car%s' % (x+1), wlans=1, ip='10.0.0.%s/8' % (x + 1), min_speed=min, max_speed=max, range=50)
+
+    bs1 = net.addBaseStation( 'RSU1', ssid= 'RSU1', mode= 'g', channel= '1', range=100 )
+    bs2 = net.addBaseStation( 'RSU2', ssid= 'RSU2', mode= 'g', channel= '6', range=100 )
+    bs3 = net.addBaseStation( 'RSU3', ssid= 'RSU3', mode= 'g', channel= '11', range=100 )
+    bs4 = net.addBaseStation( 'RSU4', ssid= 'RSU4', mode= 'g', channel= '11', range=100 )
     c1 = net.addController( 'c1', controller=Controller )
 
+    net.meshRouting('custom') 
+
     print "*** Associating and Creating links"
-    net.addMesh(car1, ssid='mesh')
-    net.addMesh(car2, ssid='mesh')
-    net.addMesh(car3, ssid='mesh')
-    net.addMesh(car4, ssid='mesh')
-    net.addMesh(car5, ssid='mesh')
     net.addLink(bs1, bs2)
     net.addLink(bs1, bs3)
-       
+    net.addLink(bs1, bs4)
+
     print "*** Starting network"
     net.build()
     c1.start()
     bs1.start( [c1] )
     bs2.start( [c1] )
     bs3.start( [c1] )
-    
+    bs4.start( [c1] )
+    i = 201
+    for sw in net.vehicles:
+        sw.start([c1])
+        os.system('ifconfig %s 10.0.0.%s' % (sw, i))
+        i+=1
+
     """uncomment to plot graph"""
     net.plotGraph(max_x=500, max_y=500)
 
     """Number of Roads"""
-    net.roads(4)
-
-    """Seed"""
-    net.seed(20) 
+    net.roads(20)
 
     """Start Mobility"""
     net.startMobility(startTime=0)
-   
+
+    i = 1
+    j = 2
+    k = 1
+    for c in car:
+        c.cmd('ifconfig %s-wlan0 192.168.0.%s/24 up' % (c,k))
+        c.cmd('ifconfig %s-eth0 192.168.1.%s/24 up' % (c,i))
+        c.cmd('ip route add 10.0.0.0/8 via 192.168.1.%s' % j)
+        i+=2
+        j+=2
+        k+=1
+
+    i = 1
+    j = 2
+    for v in net.vehiclesSTA:
+        v.cmd('ifconfig %s-eth0 192.168.1.%s/24 up' % (v, j))
+        v.cmd('ifconfig %s-mp0 10.0.0.%s/24 up' % (v,i))
+        v.cmd('echo 1 > /proc/sys/net/ipv4/ip_forward')
+        i+=1    
+        j+=2
+
+    for v1 in net.vehiclesSTA:
+        i = 1
+        j = 1
+        for v2 in net.vehiclesSTA:
+            if v1 != v2: 
+                v1.cmd('route add -host 192.168.1.%s gw 10.0.0.%s' % (j,i))
+            i+=1
+            j+=2
+
     print "*** Running CLI"
     CLI( net )
 
