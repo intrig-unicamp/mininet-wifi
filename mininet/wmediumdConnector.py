2134a677f6932e2223dc14b439c2c320063230c6
Patrick Grosse <patrick.grosse@uni-muenster.de>
2016-11-25 18:56:30 +0100

    Restored wmediumd after being overriden

--- mininet/wmediumdConnector.py
+++ mininet/wmediumdConnector.py
@@ -5,48 +5,103 @@ author: Patrick Grosse (patrick.grosse@uni-muenster.de)
 """
 
 import os
-import subprocess
 import tempfile
+
+from link import Intf
 from mininet.log import info
+from wifiModule import module
 
 
 class WmediumdConn(object):
+    is_initialized = False
+    intfrefs = None
+    links = None
+    executable = None
+    auto_add_links = None
+    default_auto_snr = None
+
     is_connected = False
-    used_tmux = False
     wmd_process = None
     wmd_config_name = None
 
     @classmethod
-    def connect_wmediumd(cls, intfrefs, links, executable='wmediumd', auto_add_links=True,
-                         default_auto_snr=0, use_tmux=False):
+    def set_wmediumd_data(cls, intfrefs, links, executable='wmediumd', auto_add_links=True,
+                          default_auto_snr=0):
         """
-        Start the wmediumd process
+        Set the data for the wmediumd daemon
 
         :param intfrefs: A list of all WmediumdIntfRef that should be managed in wmediumd
         :param links: A list of WmediumdLink
         :param executable: The wmediumd executable
         :param auto_add_links: If true, it will add all missing links pairs with the default_auto_snr as SNR
         :param default_auto_snr: The default SNR
-        :param use_tmux: True if a tmux session should be used (for debugging)
+
+        :type intfrefs: list of WmediumdIntfRef
+        :type links: list of WmediumdLink
         """
+        cls.intfrefs = intfrefs
+        cls.links = links
+        cls.executable = executable
+        cls.auto_add_links = auto_add_links
+        cls.default_auto_snr = default_auto_snr
+        cls.is_initialized = True
+
+    @classmethod
+    def connect_wmediumd_on_startup(cls):
+        """
+        This method can be called before initializing the Mininet net
+        to prevent the stations from reaching each other from the beginning.
+
+        Alternative: Call connect_wmediumd() when the net is built
+        """
+        if not cls.is_initialized:
+            raise Exception("Use set_wmediumd_data first to set the required data")
+
         if cls.is_connected:
             raise Exception('wmediumd is already initialized')
-        cls.used_tmux = use_tmux
+
+        orig_ifaceassign = module.assignIface
+
+        @classmethod
+        def intercepted_assign(other_cls, wifiNodes, physicalWlan, phyList):
+            orig_ifaceassign(wifiNodes, physicalWlan, phyList)
+            cls.connect_wmediumd_after_startup()
+
+        module.assignIface = intercepted_assign
+
+    @classmethod
+    def connect_wmediumd_after_startup(cls):
+        """
+        This method can be called after initializing the Mininet net
+        to prevent the stations from reaching each other.
+
+        The stations can reach each other before this method is called and
+        some scripts may use some kind of a cache (eg. iw station dump)
+
+        Alternative: Call intercept_module_loading() before the net is built
+        """
+        if not cls.is_initialized:
+            raise Exception("Use set_wmediumd_data first to set the required data")
+
+        if cls.is_connected:
+            raise Exception('wmediumd is already initialized')
 
         mappedintfrefs = {}
         mappedlinks = {}
 
         # Map all links using the interface identifier and check for missing interfaces in the  intfrefs list
-        for link in links:
+        for link in cls.links:
             link_id = link.sta1intfref.identifier() + '/' + link.sta2intfref.identifier()
             mappedlinks[link_id] = link
             found1 = False
             found2 = False
-            for intfref in intfrefs:
-                if link.sta1intfref.sta == intfref.sta and link.sta1intfref.sta == intfref.sta:
-                    found1 = True
-                if link.sta2intfref.sta == intfref.sta and link.sta2intfref.sta == intfref.sta:
-                    found2 = True
+            for intfref in cls.intfrefs:
+                if link.sta1intfref.get_station_name() == intfref.get_station_name():
+                    if link.sta1intfref.get_station_name() == intfref.get_station_name():
+                        found1 = True
+                if link.sta2intfref.get_station_name() == intfref.get_station_name():
+                    if link.sta2intfref.get_station_name() == intfref.get_station_name():
+                        found2 = True
             if not found1:
                 raise Exception('%s is not part of the managed interfaces' % link.sta1intfref.identifier())
                 pass
@@ -54,12 +109,12 @@ class WmediumdConn(object):
                 raise Exception('%s is not part of the managed interfaces' % link.sta2intfref.identifier())
 
         # Auto add links
-        if auto_add_links:
-            for intfref1 in intfrefs:
-                for intfref2 in intfrefs:
+        if cls.auto_add_links:
+            for intfref1 in cls.intfrefs:
+                for intfref2 in cls.intfrefs:
                     if intfref1 != intfref2:
                         link_id = intfref1.identifier() + '/' + intfref2.identifier()
-                        mappedlinks.setdefault(link_id, WmediumdLink(intfref1, intfref2, default_auto_snr))
+                        mappedlinks.setdefault(link_id, WmediumdLink(intfref1, intfref2, cls.default_auto_snr))
 
         # Create wmediumd config
         wmd_config = tempfile.NamedTemporaryFile(prefix='mn_wmd_config_', suffix='.cfg', delete=False)
@@ -67,10 +122,10 @@ class WmediumdConn(object):
         info("Name of wmediumd config: %s\n" % cls.wmd_config_name)
         configstr = 'ifaces :\n{\n\tids = ['
         intfref_id = 0
-        for intfref in intfrefs:
+        for intfref in cls.intfrefs:
             if intfref_id != 0:
                 configstr += ', '
-            grepped_mac = cls.grep_mac(intfref.sta, intfref.intf)
+            grepped_mac = intfref.get_intf_mac()
             configstr += '"%s"' % grepped_mac
             mappedintfrefs[intfref.identifier()] = intfref_id
             intfref_id += 1
@@ -91,44 +146,24 @@ class WmediumdConn(object):
         wmd_config.close()
 
         # Start wmediumd using the created config
-        if use_tmux:
-            os.system('tmux new -s mnwmd -d')
-            os.system('tmux send-keys \'%s -c %s\' C-m' % (executable, cls.wmd_config_name))
-        else:
-            cls.wmd_process = subprocess.Popen([executable, '-c', cls.wmd_config_name], stdout=subprocess.PIPE,
-                                               stderr=subprocess.STDOUT)
+        os.system('tmux new -s mnwmd -d')
+        os.system('tmux send-keys -t mnwmd \'%s -c %s\' C-m' % (cls.executable, cls.wmd_config_name))
         cls.is_connected = True
 
     @classmethod
     def disconnect_wmediumd(cls):
         """
-        Kill the wmediumd process if running
+        Kill the wmediumd process if running and delete the config
         """
         if cls.is_connected:
             try:
                 os.remove(cls.wmd_config_name)
-            except Exception:
+            except OSError:
                 pass
-            if cls.used_tmux:
-                os.system('tmux kill-session -t mnwmd')
-            elif cls.wmd_process:
-                cls.wmd_process.kill()
+            os.system('tmux kill-session -t mnwmd')
             cls.is_connected = False
         else:
             raise Exception('wmediumd is not initialized')
-
-    @staticmethod
-    def grep_mac(sta, intf):
-        """
-        Gets the MAC address of an interface of a station through ifconfig
-
-        :param sta: Mininet-Wifi station
-        :param intf: Name of the interface
-        :return: The MAC address
-        """
-        output = sta.cmd(['ifconfig', intf.name, '|', 'grep', '-o', '-E',
-                          '\'([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}\''])
-        return output.strip()
 
 
 class WmediumdLink(object):
@@ -139,6 +174,9 @@ class WmediumdLink(object):
         :param sta1intfref: Instance of WmediumdIntfRef
         :param sta2intfref: Instance of WmediumdIntfRef
         :param snr: Signal Noise Ratio as int
+
+        :type sta1intfref: WmediumdIntfRef
+        :type sta2intfref: WmediumdIntfRef
         """
         self.sta1intfref = sta1intfref
         self.sta2intfref = sta2intfref
@@ -146,20 +184,102 @@ class WmediumdLink(object):
 
 
 class WmediumdIntfRef(object):
-    def __init__(self, sta, intf):
+    def get_station_name(self):
         """
-        An unambiguous reference to an interface of a station
+        Get the name of the station
 
-        :param sta: Mininet-Wifi station
-        :param intf: Mininet interface
+        :rtype: str
         """
-        self.sta = sta
-        self.intf = intf
+        pass
+
+    def get_intf_name(self):
+        """
+        Get the interface name
+
+        :rtype: str
+        """
+        pass
+
+    def get_intf_mac(self):
+        """
+        Get the MAC address of the interface
+
+        :rtype: str
+        """
+        pass
 
     def identifier(self):
         """
         Identifier used in dicts
 
-        :return: string
+        :return: str
         """
-        return self.sta.name + "." + self.intf.name
+        return self.get_station_name() + "." + self.get_intf_name()
+
+
+class DynamicWmediumdIntfRef(WmediumdIntfRef):
+    def __init__(self, sta, intf=None):
+        """
+        An unambiguous reference to an interface of a station
+
+        :param sta: Mininet-Wifi station
+        :param intf: Mininet interface or name of Mininet interface. If None, the default interface will be used
+
+        :type sta: Station
+        :type intf: (Intf, str, None)
+        """
+        self.__sta = sta
+        self.__intf = intf
+
+    def get_station_name(self):
+        return self.__sta.name
+
+    def get_intf_name(self):
+        if isinstance(self.__intf, Intf):
+            return self.__intf.name
+        elif not self.__intf:
+            return self.__sta.params['wlan'][0]
+        elif isinstance(self.__intf, str):
+            return self.__intf
+        elif isinstance(self.__intf, int):
+            return self.__sta.params['wlan'][self.__intf]
+
+    def get_intf_mac(self):
+        intf_name = self.get_intf_name()
+        index = 0
+        found = False
+        for wlan_intf in self.__sta.params['wlan']:
+            if wlan_intf == intf_name:
+                found = True
+                break
+            index += 1
+        if found:
+            return self.__sta.params['mac'][index]
+
+
+class StaticWmediumdIntfRef(WmediumdIntfRef):
+    def __init__(self, staname, intfname, intfmac):
+        """
+        An unambiguous reference to an interface of a station
+
+        :param staname: Station name
+        :param intfname: Interface name
+        :param intfmac: Interface MAC address
+
+        :type staname: str
+        :type intfname: str
+        :type intfmac: str
+        """
+        self.__staname = staname
+        self.__intfname = intfname
+        self.__intfmac = intfmac
+
+    def get_station_name(self):
+        return self.__staname
+
+    def get_intf_name(self):
+        return self.__intfname
+
+    def get_intf_mac(self):
+        return self.__intfmac
+
