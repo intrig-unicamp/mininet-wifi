ddc21c3a3fa4fdda7bf9080ae5f27f71a44faf78
Patrick Grosse <patrick.grosse@uni-muenster.de>
2017-01-09 13:04:47 +0100

    Changed variable name apcommand => cmd

--- mininet/net.py
+++ mininet/net.py
@@ -730,8 +730,8 @@ class Mininet(object):
             \ndevice_type=1-0050F204-1\
             \np2p_no_group_iface=1' % (sta)
         confname = "mn%d_%s_wifiDirect.conf" % (os.getpid(), sta)
-        apcommand = cmd + ("\' > %s" % confname)
-        os.system(apcommand)
+        cmd = cmd + ("\' > %s" % confname)
+        os.system(cmd)
         sta.cmd('wpa_supplicant -B -Dnl80211 -c%s -i%s-wlan0 -d' % (confname, sta))
 
         value = deviceDataRate(sta=sta, wlan=wlan)
