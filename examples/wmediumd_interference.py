6a00557593f49ed0737759761a73c109119fd540
Ramon Fontes <ramonreisfontes@gmail.com>
2017-04-16 16:08:03 -0300

    update wmediumd examples

--- examples/wmediumd_interference.py
+++ examples/wmediumd_interference.py
@@ -16,10 +16,10 @@ def topology():
     net = Mininet( controller=Controller, link=TCLink, accessPoint=OVSKernelAP, useWmediumd=True, enable_interference=True )
 
     print "*** Creating nodes"
+    ap1 = net.addAccessPoint( 'ap1', ssid='new-ssid', mode='a', channel= '36', range=100, position='15,30,0' )
     sta1 = net.addStation( 'sta1', mac='00:00:00:00:00:02', ip='10.0.0.1/8', position='10,20,0' )
-    sta2 = net.addStation( 'sta2', mac='00:00:00:00:00:03', ip='10.0.0.2/8', position='10,40,0' )
+    sta2 = net.addStation( 'sta2', mac='00:00:00:00:00:03', ip='10.0.0.2/8', position='100,60,0' )
     sta3 = net.addStation( 'sta3', mac='00:00:00:00:00:04', ip='10.0.0.3/8', position='10,20,10' )
-    ap1 = net.addAccessPoint( 'ap1', ssid= 'new-ssid', mode= 'g', channel= '1', position='15,30,0' )
     c1 = net.addController( 'c1', controller=Controller )
 
     print "*** Configuring wifi nodes"
--- examples/wmediumd_mobility.py
+++ examples/wmediumd_mobility.py
@@ -18,9 +18,9 @@ def topology():
     net = Mininet( controller=Controller, link=TCLink, accessPoint=OVSKernelAP, useWmediumd=True )
 
     print "*** Creating nodes"
+    ap1 = net.addAccessPoint( 'ap1', ssid='new-ssid', mode='a', channel='36', range=100, position='50,50,0' )
     sta1 = net.addStation( 'sta1', mac='00:00:00:00:00:02', ip='10.0.0.2/8' )
     sta2 = net.addStation( 'sta2', mac='00:00:00:00:00:03', ip='10.0.0.3/8' )
-    ap1 = net.addAccessPoint( 'ap1', ssid= 'new-ssid', mode= 'g', channel= '1', position='50,50,0' )
     c1 = net.addController( 'c1', controller=Controller )
 
     print "*** Configuring wifi nodes"
