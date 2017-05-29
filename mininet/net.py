61aaebf34fde505506b9a2e88949d393b3ee57d4
Ramon Fontes <ramonreisfontes@gmail.com>
2017-05-29 15:06:08 -0300

    change to version 2.1

--- mininet/net.py
+++ mininet/net.py
@@ -118,7 +118,7 @@ from mininet.wifiNet import mininetWiFi
 from __builtin__ import True
 
 # Mininet version: should be consistent with README and LICENSE
-VERSION = "2.0r4"
+VERSION = "2.1"
 
 class Mininet(object):
     "Network emulation with hosts spawned in network namespaces."
