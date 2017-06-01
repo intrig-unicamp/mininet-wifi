e6142aef5b07b3375452e22c89f2b7b95d2208c1
Ramon Fontes <ramonreisfontes@gmail.com>
2017-06-01 20:58:20 -0300

    add test for replay mobility and change its code

--- examples/replaying/replayingMobility.py
+++ examples/replaying/replayingMobility.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 
-"""
-Replaying Mobility
-"""
+'Replaying Mobility'
 
 from mininet.net import Mininet
-from mininet.node import Controller,OVSKernelSwitch
+from mininet.node import Controller, OVSAP
 from mininet.link import TCLink
 from mininet.cli import CLI
 from mininet.log import setLogLevel
@@ -14,50 +12,61 @@ from mininet.wifiReplaying import replayingMobility
 def topology():
 
     "Create a network."
-    net = Mininet( controller=Controller, link=TCLink, switch=OVSKernelSwitch )
+    net = Mininet(controller=Controller, link=TCLink, accessPoint=OVSAP, enable_wmediumd=True, enable_interference=True)
 
     print "*** Creating nodes"
-    sta1 = net.addStation( 'sta1', mac='00:00:00:00:00:02', ip='10.0.0.2/8', speed=1 )
-    sta2 = net.addStation( 'sta2', mac='00:00:00:00:00:03', ip='10.0.0.3/8', speed=3 )
-    ap1 = net.addAccessPoint( 'ap1', ssid= 'new-ssid', mode= 'g', channel= '1', position='50,50,0' )
-    c1 = net.addController( 'c1', controller=Controller )
+    sta1 = net.addStation('sta1', mac='00:00:00:00:00:02', ip='10.0.0.1/8', speed=4)
+    sta2 = net.addStation('sta2', mac='00:00:00:00:00:03', ip='10.0.0.2/8', speed=6)
+    sta3 = net.addStation('sta3', mac='00:00:00:00:00:04', ip='10.0.0.3/8', speed=3)
+    sta4 = net.addStation('sta4', mac='00:00:00:00:00:05', ip='10.0.0.4/8', speed=3)
+    ap1 = net.addAccessPoint('ap1', ssid='new-ssid', mode='g', channel='1', position='45,45,0')
+    c1 = net.addController('c1', controller=Controller)
+
+    print "*** Configuring Propagation Model"
+    net.propagationModel("logDistancePropagationLossModel", exp=4.5)
 
     print "*** Configuring wifi nodes"
     net.configureWifiNodes()
 
+    print "*** Creating links"
+    net.addHoc(sta3, ssid='adhocNet')
+    net.addHoc(sta4, ssid='adhocNet')
+
     print "*** Starting network"
     net.build()
     c1.start()
-    ap1.start( [c1] )
+    ap1.start([c1])
 
     """uncomment to plot graph"""
-    net.plotGraph(max_x=100, max_y=100)
+    net.plotGraph(max_x=200, max_y=200)
 
     getTrace(sta1, 'examples/replaying/replayingMobility/node1.dat')
     getTrace(sta2, 'examples/replaying/replayingMobility/node2.dat')
+    getTrace(sta3, 'examples/replaying/replayingMobility/node3.dat')
+    getTrace(sta4, 'examples/replaying/replayingMobility/node4.dat')
 
     replayingMobility(net)
 
     print "*** Running CLI"
-    CLI( net )
+    CLI(net)
 
     print "*** Stopping network"
     net.stop()
 
-def getTrace(sta, file):
+def getTrace(sta, file_):
 
-    file = open(file, 'r')
-    raw_data = file.readlines()
-    file.close()
-    
+    file_ = open(file_, 'r')
+    raw_data = file_.readlines()
+    file_.close()
+
     sta.position = []
 
     for data in raw_data:
         line = data.split()
-        x = line[0] #First Column
-        y = line[1] #Second Column
+        x = line[0]  # First Column
+        y = line[1]  # Second Column
         sta.position.append('%s,%s,0' % (x, y))
 
 if __name__ == '__main__':
-    setLogLevel( 'info' )
+    setLogLevel('info')
     topology()
