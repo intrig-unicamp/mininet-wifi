b438e3612adc766d21a6d5f1896dcf23b2c65966
Ramon Fontes <ramonreisfontes@gmail.com>
2017-07-20 13:39:04 -0300

    update code

--- examples/wifiDirect.py
+++ examples/wifiDirect.py
@@ -10,11 +10,14 @@ from mininet.link import TCLink
 
 def topology():
     "Create a network."
-    net = Mininet(controller=Controller, link=TCLink)
+    net = Mininet(controller=Controller, link=TCLink, enable_wmediumd=True, enable_interference=True)
 
     print "*** Creating nodes"
-    sta1 = net.addStation('sta1', ip='10.0.0.1/8')
-    sta2 = net.addStation('sta2', ip='10.0.0.2/8')
+    sta1 = net.addStation('sta1', ip='10.0.0.1/8', position='10,10,0')
+    sta2 = net.addStation('sta2', ip='10.0.0.2/8', position='40,40,0')
+
+    print "*** Configuring Propagation Model"
+    net.propagationModel("logDistancePropagationLossModel", exp=3.5)
 
     print "*** Configuring wifi nodes"
     net.configureWifiNodes()
@@ -25,6 +28,19 @@ def topology():
 
     print "*** Starting network"
     net.build()
+
+    "Plotting graph"
+    net.plotGraph(max_x=200, max_y=200)
+
+    sta1.cmd('wpa_cli -ista1-wlan0 p2p_find')
+    sta2.cmd('wpa_cli -ista2-wlan0 p2p_find')
+    sta2.cmdPrint('wpa_cli -ista2-wlan0 p2p_peers')
+    time.sleep(2)
+    sta1.cmdPrint('wpa_cli -ista1-wlan0 p2p_peers')
+    time.sleep(2)
+    pin = sta1.cmd('wpa_cli -ista1-wlan0 p2p_connect 02:00:00:00:01:00 pin auth')
+    time.sleep(3)
+    sta2.cmd('wpa_cli -ista2-wlan0 p2p_connect 02:00:00:00:00:00 %s' % pin)
 
     print "*** Running CLI"
     CLI(net)
