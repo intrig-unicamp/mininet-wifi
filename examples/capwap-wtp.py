cee3788ce313c41f6f3a71446595630832063380
Ramon Fontes <ramonreisfontes@gmail.com>
2017-08-08 15:16:14 -0300

    add example for capwap-wtp

---
+++ examples/capwap-wtp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+"""Enabling CAPWAP_WTP"""
+
+from mininet.net import Mininet
+from mininet.node import Controller, OVSKernelAP
+from mininet.link import TCLink
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+
+def topology():
+
+    "Create a network."
+    net = Mininet(controller=Controller, link=TCLink, accessPoint=OVSKernelAP, \
+		driver='capwap_wtp', enable_wmediumd=True, enable_interference=True)
+
+    print "*** Creating nodes"
+    ap1 = net.addAccessPoint('ap1', ssid='new-ssid', mode='g', channel='1', position='15,30,0')
+    sta1 = net.addStation('sta1', mac='00:00:00:00:00:02', ip='10.0.0.1/8', position='10,20,0')
+    sta2 = net.addStation('sta2', mac='00:00:00:00:00:03', ip='10.0.0.2/8', position='20,20,0')
+    c1 = net.addController('c1', controller=Controller)
+
+    print "*** Configuring Propagation Model"
+    net.propagationModel("logDistancePropagationLossModel", exp=4)
+
+    print "*** Configuring wifi nodes"
+    net.configureWifiNodes()
+
+    print "*** Starting network"
+    net.build()
+    c1.start()
+    ap1.start([c1])
+
+    """plotting graph"""
+    net.plotGraph(max_x=100, max_y=100)
+
+    print "*** Running CLI"
+    CLI(net)
+
+    print "*** Stopping network"
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('debug')
+    topology()
