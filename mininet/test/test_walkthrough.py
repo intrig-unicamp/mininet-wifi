c3c28462f8a44814f8dc2e0fa744e96fcc0cfb7f
Ramon Fontes <ramonreisfontes@gmail.com>
2017-09-13 09:33:42 -0300

    increase sleep time

--- mininet/test/test_walkthrough.py
+++ mininet/test/test_walkthrough.py
@@ -222,7 +222,7 @@ class testWalkthrough(unittest.TestCase):
         "Start Mininet-WiFi with wifi direct, then test ping"
         p = pexpect.spawn(
             'python examples/wifiDirect.py')
-        sleep(12)
+        sleep(15)
         p.sendline('sta1 ping -c 1 sta2')
         p.expect('1 packets transmitted, 1 received')
         p.expect(self.prompt)
