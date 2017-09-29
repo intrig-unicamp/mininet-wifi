2dc315bd62340d60330874d665cdc484f00ef213
Ramon Fontes <ramonreisfontes@gmail.com>
2017-09-29 18:18:44 -0300

    increase max_x and max_y

--- examples/wmediumd_mobility.py
+++ examples/wmediumd_mobility.py
@@ -36,7 +36,7 @@ def topology():
     net.seed(20)
 
     "*** Available models: RandomWalk, TruncatedLevyWalk, RandomDirection, RandomWayPoint, GaussMarkov, ReferencePoint, TimeVariantCommunity ***"
-    net.startMobility(time=0, model='RandomDirection', max_x=100, max_y=100, min_v=0.5, max_v=0.8)
+    net.startMobility(time=0, model='RandomDirection', max_x=300, max_y=300, min_v=0.5, max_v=0.8)
 
     print "*** Starting network"
     net.build()
