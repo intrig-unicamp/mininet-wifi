ebff2e0d4373e0e2e8177d1638b1e7ec1f9d685b
Eduardo Soares <eduardosoares.pt@gmail.com>
2017-11-02 19:49:35 +0000

    Add in propagation models utility class to calculate power of nodes according to a range
    Updates adhoc example to use this utility class to calculate a txpower
    adjusted to range

--- examples/adhoc.py
+++ examples/adhoc.py
@@ -4,9 +4,11 @@
 
 sta1 <---> sta2 <---> sta3"""
 
-from mininet.net import Mininet
 from mininet.cli import CLI
 from mininet.log import setLogLevel
+from mininet.net import Mininet
+from mininet.wifiPropagationModels import powerForRangeByPropagationModel
+
 
 def topology():
     "Create a network."
@@ -14,10 +16,18 @@ def topology():
 
     print "*** Creating nodes"
     sta1 = net.addStation('sta1', position='10,10,0')
-    sta2 = net.addStation('sta2', position='110,10,0')
-    sta3 = net.addStation('sta3', position='210,10,0')
+    sta2 = net.addStation('sta2', position='50,10,0')
+    sta3 = net.addStation('sta3', position='90,10,0')
 
     net.propagationModel("logDistancePropagationLossModel", exp=3.5)
+
+    # calc for sta1, the others should be the same
+    power_to_distance = powerForRangeByPropagationModel().logDistancePropagationLossModel(node=sta1, wlan=0,
+                                                                                          distance=50)
+
+    sta1.setTxPower(sta1.params['wlan'][0], power_to_distance)
+    sta2.setTxPower(sta2.params['wlan'][0], power_to_distance)
+    sta3.setTxPower(sta3.params['wlan'][0], power_to_distance)
 
     print "*** Configuring wifi nodes"
     net.configureWifiNodes()
@@ -26,6 +36,9 @@ def topology():
     net.addHoc(sta1, ssid='adhocNet', mode='g', channel=5)
     net.addHoc(sta2, ssid='adhocNet', mode='g', channel=5)
     net.addHoc(sta3, ssid='adhocNet', mode='g', channel=5)
+
+    print "*** Plot Graph"
+    net.plotGraph(max_x=100, max_y=100)
 
     print "*** Starting network"
     net.build()
@@ -36,7 +49,7 @@ def topology():
     print "*** Stopping network"
     net.stop()
 
+
 if __name__ == '__main__':
     setLogLevel('info')
     topology()
-
