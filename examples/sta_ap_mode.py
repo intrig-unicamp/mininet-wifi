96646c77d8e24ae02a7bfbc4c19627f7b1d3faa2
Ramon Fontes <ramonreisfontes@gmail.com>
2017-11-23 09:13:32 -0300

    adjust node position

--- examples/sta_ap_mode.py
+++ examples/sta_ap_mode.py
@@ -43,7 +43,7 @@ def topology(mobility):
     if mobility:
         net.startMobility(time=1)
         net.mobility(sta1, 'start', time=1, position='20.0,60.0,0.0')
-        net.mobility(sta1, 'stop', time=5, position='90.0,60.0,0.0')
+        net.mobility(sta1, 'stop', time=5, position='70.0,60.0,0.0')
         net.stopMobility(time=6)
 
     print "*** Starting network"
