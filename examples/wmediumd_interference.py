8ca620e580df1622436d75ec105c04bd0dade60b
Ramon Fontes <ramonreisfontes@gmail.com>
2017-12-04 16:14:50 -0300

    fix typo

--- examples/wmediumd_interference.py
+++ examples/wmediumd_interference.py
@@ -16,7 +16,7 @@ def topology():
     "Create a network."
     net = Mininet(controller=Controller, link=TCLink, accessPoint=OVSKernelAP,
                   enable_wmediumd=True, enable_interference=True, noise_threshold=-91,
-                  fading_coefficient=1gigi)
+                  fading_coefficient=1)
 
     print "*** Creating nodes"
     ap1 = net.addAccessPoint('ap1', ssid='new-ssid', mode='a', channel='36',
