9ddb8430956a3faf200bad8bcbbcd0d0f29d3345
Ramon Fontes <ramonreisfontes@gmail.com>
2018-01-01 18:04:56 -0300

    fixes for getRange()

--- mininet/wifi/node.py
+++ mininet/wifi/node.py
@@ -250,9 +250,9 @@ class WiFiNode(object):
         if not isinstance(self, Station) and not isinstance(self, Car) \
                 and not isinstance(self, AP):
             self = self.params['associatedTo'][0]
-        value = int(distanceByPropagationModel(self, wlan, enable_interference))
+        value = distanceByPropagationModel(self, wlan, enable_interference)
 
-        return value.dist
+        return int(value.dist)
 
     def updateGraph(self):
         "Update the Graph"
