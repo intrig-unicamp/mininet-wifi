e4d987837421a349bde2f098d7924f0ea3f5dc0d
Nick Gordon <nmgordon@memphis.edu>
2018-04-16 13:57:35 -0500

    Updated adding adhoc links to mininet objects.

--- mininet/wifi/net.py
+++ mininet/wifi/net.py
@@ -538,6 +538,9 @@ class Mininet_wifi(Mininet):
                         value = self.getDataRate(sta, ap, wlan)
                         bw = value.rate
                         params['bw'] = bw
+                        if self.ifb:
+                            params['ifb'] = True
+                            params['ifb_intf'] = wlan
                     # tc = True, this is useful only to apply tc configuration
                     link = cls(name=sta.params['wlan'][wlan], node=sta,
                                tc=True, **params)
