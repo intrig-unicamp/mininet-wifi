9f6987d9070b71af16c0bf69ae8a38c5c3dad3cf
Ramon Fontes <ramonreisfontes@gmail.com>
2019-01-08 08:33:41 -0300

    add propagation model

--- examples/mesh.py
+++ examples/mesh.py
@@ -32,6 +32,9 @@ def topology(mobility):
         sta2 = net.addStation('sta2', position='50,10,0')
         sta3 = net.addStation('sta3', position='90,10,0')
 
+    info("*** Configuring Propagation Model\n")
+    net.setPropagationModel(model="logDistance", exp=4)
+
     info("*** Configuring wifi nodes\n")
     net.configureWifiNodes()
 
