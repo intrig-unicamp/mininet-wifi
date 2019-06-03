eadff9987343f4795436b3beee0bf0c75fb9d15b
Ramon Fontes <ramonreisfontes@gmail.com>
2019-06-03 13:00:02 -0300

    fix for replaying rssi

--- examples/replaying/replayingRSSI.py
+++ examples/replaying/replayingRSSI.py
@@ -3,7 +3,7 @@
 "Replaying RSSI"
 import os
 
-from mininet.node import Controller,OVSKernelSwitch
+from mininet.node import Controller
 from mininet.log import setLogLevel, info
 from mn_wifi.replaying import replayingRSSI
 from mn_wifi.cli import CLI_wifi
@@ -12,14 +12,14 @@ from mn_wifi.net import Mininet_wifi
 
 def topology():
     "Create a network."
-    net = Mininet_wifi( controller=Controller, switch=OVSKernelSwitch )
+    net = Mininet_wifi( controller=Controller )
 
     info("*** Creating nodes\n")
     sta1 = net.addStation( 'sta1', mac='00:00:00:00:00:02', ip='10.0.0.2/8' )
     sta2 = net.addStation( 'sta2', mac='00:00:00:00:00:03', ip='10.0.0.3/8' )
     ap1 = net.addAccessPoint( 'ap1', ssid='new-ssid', mode='g', channel='1',
                               position='50,50,0' )
-    c1 = net.addController( 'c1', controller=Controller )
+    c1 = net.addController( 'c1' )
 
     info("*** Configuring wifi nodes\n")
     net.configureWifiNodes()
