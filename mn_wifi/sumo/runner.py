21f12c3184a3d8bd29b32ef422744d20bee02367
Ramon Fontes <ramonreisfontes@gmail.com>
2019-10-04 13:06:09 -0300

    sumo works only with wmediumd

--- mn_wifi/sumo/runner.py
+++ mn_wifi/sumo/runner.py
@@ -104,7 +104,8 @@ class sumo(object):
                         y2 = vehicleCmds.getPosition(vehID2)[1]
 
                         if int(vehID1) < len(cars):
-                            cars[int(vehID1)].setPosition('%s, %s, %s' % (x1, y1, 0))
+                            cars[int(vehID1)].params['position'] = x1, y1, 0
+                            cars[int(vehID1)].set_pos_wmediumd(cars[int(vehID1)].params['position'])
 
                         if abs(x1-x2)>0 and abs(x1-x2)<20 \
                                 and (road1 == opposite_road2 or road2 == opposite_road1):
