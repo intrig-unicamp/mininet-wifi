b28a4f2ac4dcd03c7330a53f0ba99303f3e4cfcb
Ramon Fontes <ramonreisfontes@gmail.com>
2019-10-11 13:59:10 -0300

    add babel and batman in the test list

--- examples/adhoc.py
+++ examples/adhoc.py
@@ -13,12 +13,12 @@ from mn_wifi.net import Mininet_wifi
 from mn_wifi.wmediumdConnector import interference
 
 
-def topology(autoTxPower):
+def topology(args):
     "Create a network."
     net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference)
 
     info("*** Creating nodes\n")
-    if autoTxPower:
+    if '-a' in args:
         sta1 = net.addStation('sta1', position='10,10,0', range=100)
         sta2 = net.addStation('sta2', position='50,10,0', range=100)
         sta3 = net.addStation('sta3', position='90,10,0', range=100)
@@ -32,22 +32,33 @@ def topology(autoTxPower):
 
     net.setPropagationModel(model="logDistance", exp=4)
 
+    #net.plotGraph(max_x=300, max_y=300)
+
     info("*** Configuring wifi nodes\n")
     net.configureWifiNodes()
 
     info("*** Creating links\n")
-    #MANET routing protocols supported by proto: babel, batman and olsr
-    net.addLink(sta1, cls=adhoc, ssid='adhocNet', #proto='olsr',
-                mode='g', channel=5, ht_cap='HT40+')
-    net.addLink(sta2, cls=adhoc, ssid='adhocNet', #proto='olsr',
-                mode='g', channel=5)
-    net.addLink(sta3, cls=adhoc, ssid='adhocNet', #proto='olsr',
-                mode='g', channel=5, ht_cap='HT40+')
+    # MANET routing protocols supported by proto: babel, batman and olsr
+    if 'babel' in args or 'batman' in args or 'olsr' in args:
+        proto = args[1]
+        net.addLink(sta1, cls=adhoc, ssid='adhocNet', proto=proto,
+                    mode='g', channel=5, ht_cap='HT40+')
+        net.addLink(sta2, cls=adhoc, ssid='adhocNet', proto=proto,
+                    mode='g', channel=5)
+        net.addLink(sta3, cls=adhoc, ssid='adhocNet', proto=proto,
+                    mode='g', channel=5, ht_cap='HT40+')
+    else:
+        net.addLink(sta1, cls=adhoc, ssid='adhocNet',
+                    mode='g', channel=5, ht_cap='HT40+')
+        net.addLink(sta2, cls=adhoc, ssid='adhocNet',
+                    mode='g', channel=5)
+        net.addLink(sta3, cls=adhoc, ssid='adhocNet',
+                    mode='g', channel=5, ht_cap='HT40+')
 
     info("*** Starting network\n")
     net.build()
 
-    #info("*** Addressing...\n")
+    info("*** Addressing...\n")
     #sta1.setIPv6('2001::1/64', intf="sta1-wlan0")
     #sta2.setIPv6('2001::2/64', intf="sta2-wlan0")
     #sta3.setIPv6('2001::3/64', intf="sta3-wlan0")
@@ -61,5 +72,4 @@ def topology(autoTxPower):
 
 if __name__ == '__main__':
     setLogLevel('info')
-    autoTxPower = True if '-a' in sys.argv else False
-    topology(autoTxPower)
+    topology(sys.argv)
