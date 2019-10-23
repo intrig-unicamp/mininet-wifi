c31acb528f086f6f8cb1df4c0b6669e7817ee328
Ramon Fontes <ramonreisfontes@gmail.com>
2019-10-23 09:56:01 -0300

    battery support: this is only the first release

---
+++ examples/battery.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+"""This example creates a simple network topology in which
+   stations are equipped with batteries"""
+
+import sys
+
+from mininet.node import Controller
+from mininet.log import setLogLevel, info
+from mn_wifi.cli import CLI_wifi
+from mn_wifi.net import Mininet_wifi
+
+
+def topology():
+    "Create a network."
+    net = Mininet_wifi()
+
+    info("*** Creating nodes\n")
+    sta1 = net.addStation('sta1', battery=100)
+    sta2 = net.addStation('sta2', battery=100)
+    ap1 = net.addAccessPoint('ap1', ssid="simplewifi", mode="g",
+                             channel="5", failMode='standalone')
+
+    info("*** Configuring wifi nodes\n")
+    net.configureWifiNodes()
+
+    info("*** Associating Stations\n")
+    net.addLink(sta1, ap1)
+    net.addLink(sta2, ap1)
+
+    info("*** Starting network\n")
+    net.build()
+    ap1.start([])
+
+    info("*** Running CLI\n")
+    CLI_wifi(net)
+
+    info("*** Stopping network\n")
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topology()
---
+++ mn_wifi/energy.py
@@ -0,0 +1,42 @@
+"""Mininet-WiFi: A simple networking testbed for Wireless OpenFlow/SDWN!
+
+   author: Ramon Fontes (ramonrf@dca.fee.unicamp.br)"""
+
+from mn_wifi.mobility import mobility
+from threading import Thread as thread
+from time import sleep
+
+
+class Energy(object):
+
+    def __init__(self, nodes):
+        mobility.thread_ = thread(target=self.start, args=(nodes,))
+        mobility.thread_.daemon = True
+        mobility.thread_._keep_alive = True
+        mobility.thread_.start()
+
+    def start(self, nodes):
+        while mobility.thread_._keep_alive:
+            sleep(1)  # set sleep time to 1 second
+            for node in nodes:
+                self.level(node)
+
+    def level(self, node):
+        # this is only an empirical approach
+        # we may need to consider tx/rx packets,
+        # txpower; etc
+        level = node.params['battery']
+        level *= 0.1
+        node.params['battery'] = level
+        if level == 0:
+            self.shutdown_intfs(node)
+
+    def shutdown_intfs(self, node):
+        # passing through all the interfaces
+        # but we need to consider interfaces separately
+        for wlan in node.params['wlans']:
+            self.ipLink(wlan)
+
+    def ipLink(self, intf):
+        "Configure ourselves using ip link"
+        self.cmd('ip link set %s down' % intt)
