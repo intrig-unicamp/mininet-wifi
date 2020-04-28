67745e943be3594fef8cad763ac4ed59fe5be871
Ramon Fontes <ramonreisfontes@gmail.com>
2020-04-28 13:44:01 -0300

    configDefault is already set for self.cars

--- mn_wifi/net.py
+++ mn_wifi/net.py
@@ -591,7 +591,7 @@ class Mininet_wifi(Mininet, Mininet_IoT):
 
     def configHosts(self):
         "Configure a set of nodes."
-        nodes = self.hosts + self.cars + self.sensors
+        nodes = self.hosts + self.sensors
         for node in nodes:
             # info( host.name + ' ' )
             intf = node.defaultIntf()
