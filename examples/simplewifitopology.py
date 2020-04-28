cc83b15c59542d799cb3bb2997e94f49767ec4e6
Ramon Fontes <ramonreisfontes@gmail.com>
2020-04-28 09:51:42 -0300

    simplewifitopology.py: some code adjustments

--- examples/simplewifitopology.py
+++ examples/simplewifitopology.py
@@ -4,29 +4,30 @@
 
 import sys
 
-from mininet.node import Controller
 from mininet.log import setLogLevel, info
 from mn_wifi.cli import CLI
 from mn_wifi.net import Mininet_wifi
 
 
-def topology(isVirtual):
+def topology():
     "Create a network."
     net = Mininet_wifi()
 
     info("*** Creating nodes\n")
-    if isVirtual:
-        sta1 = net.addStation('sta1', nvif=2)
-        ap1 = net.addAccessPoint('ap1', ssid="simpletopo",
-                                 mode="g", channel="5")
+    sta_arg = dict()
+    ap_arg = dict()
+    if '-v' in sys.argv:
+        sta_arg = {'nvif': 2}
     else:
-        sta1 = net.addStation('sta1')
         # isolate_clientes: Client isolation can be used to prevent low-level
         # bridging of frames between associated stations in the BSS.
         # By default, this bridging is allowed.
         # OpenFlow rules are required to allow communication among nodes
-        ap1 = net.addAccessPoint('ap1', ssid="simpletopo",
-                                 client_isolation=True, mode="g", channel="5")
+        ap_arg = {'client_isolation': True}
+
+    ap1 = net.addAccessPoint('ap1', ssid="simpletopo", mode="g",
+                             channel="5", **ap_arg)
+    sta1 = net.addStation('sta1', **sta_arg)
     sta2 = net.addStation('sta2')
     c0 = net.addController('c0')
 
@@ -42,7 +43,7 @@ def topology(isVirtual):
     c0.start()
     ap1.start([c0])
 
-    if not isVirtual:
+    if '-v' not in sys.argv:
         ap1.cmd('ovs-ofctl add-flow ap1 "priority=0,arp,in_port=1,'
                 'actions=output:in_port,normal"')
         ap1.cmd('ovs-ofctl add-flow ap1 "priority=0,icmp,in_port=1,'
@@ -61,5 +62,4 @@ def topology(isVirtual):
 
 if __name__ == '__main__':
     setLogLevel('info')
-    isVirtual = True if '-v' in sys.argv else False
-    topology(isVirtual)
+    topology()
