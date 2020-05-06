621306bf02b49e9d8fbc76afe62b91a35852faf7
Ramon Fontes <ramonreisfontes@gmail.com>
2020-05-06 08:01:15 -0300

    ac: fix for handover

--- mn_wifi/associationControl.py
+++ mn_wifi/associationControl.py
@@ -11,28 +11,25 @@ class AssociationControl(object):
         if ac in dir(self):
             self.__getattribute__(ac)(intf, ap_intf)
 
-    def disconnect(self, intf):
-        return 'iw dev %s disconnect' % intf.name
-
     def llf(self, intf, ap_intf):
-        #llf: Least loaded first
+        'llf: Least loaded first'
         apref = intf.associatedTo
         if apref != '':
             ref_llf = len(apref.associatedStations)
             if len(ap_intf.associatedStations) + 2 < ref_llf:
-                intf.node.pexec(self.disconnect(intf))
+                intf.disconnect()
                 self.changeAP = True
         else:
             self.changeAP = True
         return self.changeAP
 
     def ssf(self, intf, ap_intf):
-        #ssf: Strongest signal first
+        'ssf: Strongest signal first'
         dist = intf.node.get_distance_to(intf.associatedTo)
         rssi = intf.get_rssi(ap_intf, dist)
         ref_dist = intf.node.get_distance_to(ap_intf.node)
         ref_rssi = intf.get_rssi(ap_intf, ref_dist)
         if float(ref_rssi) > float(rssi + 0.1):
-            intf.node.pexec(self.disconnect(intf))
+            intf.disconnect()
             self.changeAP = True
         return self.changeAP
