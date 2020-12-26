cc7ce4b106ecb5777fd6f7f463b96ae775b903e7
Ramon Fontes <ramonreisfontes@gmail.com>
2020-12-25 23:15:00 -0300

    6Ghz: add initial support

--- mn_wifi/link.py
+++ mn_wifi/link.py
@@ -140,11 +140,11 @@ class IntfWireless(Intf):
         w_server.register_interface(self.mac)
 
     def getCustomRate(self):
-        mode_rate = {'a': 11, 'b': 3, 'g': 11, 'n': 600, 'n5': 600, 'ac': 1000}
+        mode_rate = {'a': 11, 'b': 3, 'g': 11, 'n': 600, 'n5': 600, 'ac': 1000, 'ax': 2000}
         return mode_rate.get(self.mode)
 
     def getRate(self):
-        mode_rate = {'a': 54, 'b': 11, 'g': 54, 'n': 300, 'n5': 300, 'ac': 600}
+        mode_rate = {'a': 54, 'b': 11, 'g': 54, 'n': 300, 'n5': 300, 'ac': 600, 'ax': 1200}
         return mode_rate.get(self.mode)
 
     def rec_rssi(self):
@@ -166,6 +166,22 @@ class IntfWireless(Intf):
                           169, 171, 172, 173, 174, 175, 176,
                           177, 178, 179, 180, 181, 182, 183, 184,
                           185]
+        chan_list_6ghz = [1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41,
+                          45, 49, 53, 57, 61, 65, 69, 73, 77, 81,
+                          85, 89, 93, 97, 101, 105, 109, 113, 117,
+                          121, 125, 129, 133, 137, 141, 145, 149,
+                          153, 157, 161, 165, 169, 173, 177, 181,
+                          185, 189, 193, 197, 201, 205, 209, 213,
+                          217, 221, 225, 229, 233]
+
+        freq_list_6ghz = [5.955, 5.975, 5.995, 6.015, 6.035, 6.055, 6.095,
+                          6.115, 6.135, 6.155, 6.175, 6.195, 6.215, 6.235,
+                          6.255, 6.275, 6.295, 6.315, 6.335, 6.375, 6.415,
+                          6.435, 6.455, 6.475, 6.495, 6.515, 6.535, 6.555,
+                          6.575, 6.595, 6.615, 6.635, 6.655, 6.675, 6.695,
+                          6.715, 6.735, 6.755, 6.775, 6.795, 6.815, 6.835,
+                          6.855, 6.875, 6.895, 6.915, 6.935, 6.955, 6.975,
+                          6.995, 7.015, 7.035, 7.055, 7.075, 7.095, 7.115]
         freq_list_2ghz = [2.412, 2.417, 2.422, 2.427, 2.432, 2.437,
                           2.442, 2.447, 2.452, 2.457, 2.462]
         freq_list_5ghz = [5.18, 5.2, 5.22, 5.24, 5.26, 5.28, 5.30, 5.32,
@@ -174,8 +190,8 @@ class IntfWireless(Intf):
                           5.805, 5.825, 5.845, 5.855, 5.86, 5.865, 5.87,
                           5.875, 5.88, 5.885, 5.89, 5.895, 5.9, 5.905,
                           5.91, 5.915, 5.92, 5.925]
-        all_chan = chan_list_2ghz + chan_list_5ghz
-        all_freq = freq_list_2ghz + freq_list_5ghz
+        all_chan = chan_list_2ghz + chan_list_5ghz + chan_list_6ghz
+        all_freq = freq_list_2ghz + freq_list_5ghz + freq_list_6ghz
         if int(channel) in all_chan:
             idx = all_chan.index(int(channel))
             return all_freq[idx]
@@ -188,7 +204,7 @@ class IntfWireless(Intf):
         return int(format(self.freq, '.3f').replace('.', ''))
 
     def setReg(self):
-        if self.mode == 'a' or self.mode == 'ac':
+        if self.mode == 'a' or self.mode == 'ac' or self.mode == 'ax':
             self.pexec('iw reg set US')
 
     def setIntfName(self, *args):
@@ -230,8 +246,6 @@ class IntfWireless(Intf):
         if not wmediumd_mode.mode:
             bw = self.params['bw'][wlan] if 'bw' in self.params else self.getBW()
 
-            print(("tc qdisc replace dev %s root handle 2: tbf rate %sMbit "
-                     "burst 15000 latency 1ms" % (self.name, bw)))
             self.cmd("tc qdisc replace dev %s root handle 2: tbf rate %sMbit "
                      "burst 15000 latency 1ms" % (self.name, bw))
             # Reordering packets
