154ad15c44594d131ac63d8c452fbd4c664b76c9
Joe B <95513994+JsphByd@users.noreply.github.com>
2024-05-20 12:23:51 -0600

    Update Joes_setup.py

--- Joes_setup.py
+++ Joes_setup.py
@@ -5,3 +5,7 @@ os.system("sudo util/install.sh -Wlnfv")
 os.system("sudo apt install mininet")
 
 os.system("sudo make install")
+
+os.system("sudo apt install openvswitch-testcontroller")
+
+os.system("sudo ln /usr/bin/ovs-testcontroller /usr/bin/controller")
